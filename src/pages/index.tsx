ea308b426604a160938fbc1d70a1779b0be581eb
Gautam <gautam.gv@somaiya.edu>
2023-11-04 23:53:42 +0530

    added a simple questions section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import { Button } from '@nexus-ds/react'
+import { Avatar, Button, Chip, ScrollShadow } from '@nexus-ds/react'
+import Marquee from 'react-fast-marquee'
 
 export default function Home() {
-  return <HeroSection />
+  return (
+    <>
+      <HeroSection />
+      <SampleQuestionsSection />
+    </>
+  )
 }
 
 function HeroSection() {
@@ -22,5 +28,81 @@ function HeroSection() {
         Try Nexus Designer
       </Button>
     </div>
+  )
+}
+
+function SampleQuestionsSection() {
+  return (
+    <div
+      className={`h-screen w-full p-4 flex flex-col gap-8 items-center justify-center`}
+    >
+      <QuestionsStrip />
+      <QuestionsStrip direction={'right'} />
+      <QuestionsStrip />
+    </div>
+  )
+}
+
+interface IQuestionsStripProps {
+  direction?: 'left' | 'right'
+}
+
+function QuestionsStrip(props: IQuestionsStripProps) {
+  const direction = props.direction ?? 'left'
+
+  const questions: IQuestionChipProps[] = [
+    {
+      thumbnail: 'foo',
+      question:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry?'
+    },
+    {
+      thumbnail: 'foo',
+      question:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry?'
+    },
+    {
+      thumbnail: 'foo',
+      question:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry?'
+    },
+    {
+      thumbnail: 'foo',
+      question:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry?'
+    }
+  ]
+
+  return (
+    <Marquee
+      direction={direction}
+      className={`
+        before:h-full before:absolute before:w-16 before:bg-gradient-to-r before:from-background before:to-transparent before:z-10
+        after:right-0 after:h-full after:absolute after:w-16 after:bg-gradient-to-l after:from-background after:to-transparent after:z-10
+     `}
+    >
+      {questions.map((q, i) => (
+        <QuestionChip
+          key={i}
+          {...q}
+        />
+      ))}
+    </Marquee>
+  )
+}
+
+interface IQuestionChipProps {
+  thumbnail: String
+  question: String
+}
+
+function QuestionChip(props: IQuestionChipProps) {
+  return (
+    <Chip
+      avatar={<Avatar />}
+      className={'mx-4 p-8 bg-neutral-800'}
+    >
+      <span className={'text-2xl font-extralight'}>{props.question}</span>
+    </Chip>
   )
 }
