4ad13a8b938d2fa590224873e48a3075456c46ce
Gautam <gautam.gv@somaiya.edu>
2023-11-04 23:51:46 +0530

    added the inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 
 import { NexusUIProvider } from '@nexus-ds/react'
 import Layout from './Layout'
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '500', '600', '800', '900']
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NexusUIProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <div className={inter.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
     </NexusUIProvider>
   )
 }
