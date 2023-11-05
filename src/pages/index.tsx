818d3072eb3c030eda376515eceb87693e794300
Gautam <gautam.gv@somaiya.edu>
2023-11-05 17:55:43 +0530

    hero section now has an image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Avatar, Button, Chip, Image } from '@nexus-ds/react'
 import Marquee from 'react-fast-marquee'
 
 // static assets
+import Landing from '@/assets/img/landing.jpg'
 import Screenshot from '@/assets/img/screenshot.jpg'
 import Logo1 from '@/assets/img/logo/1.png'
 import Logo2 from '@/assets/img/logo/2.png'
@@ -24,20 +25,35 @@ export default function Home() {
 function HeroSection() {
   return (
     <div
-      className={`h-screen -mt-16 w-full md:w-1/2 p-4 m-auto flex flex-col gap-8 items-center justify-center`}
+      className={`h-screen -mt-16 w-full flex flex-col items-center justify-center`}
     >
-      <h1 className={`text-6xl text-center`}>The AI design assistant</h1>
-      <p className={`text-xl text-foreground-300`}>
-        Get answers to all your design needs
-      </p>
-      <Button
-        radius='full'
-        variant='ghost'
-        color='primary'
-        size='lg'
+      {/*Background image*/}
+      <div
+        className={`absolute w-full h-full top-0 left-0 -z-10`}
+        style={{
+          backgroundImage: `url(${Landing.src})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+          filter: 'brightness(0.2)'
+        }}
+      ></div>
+
+      <div
+        className={`w-full md:w-1/2 p-4 flex flex-col gap-8 items-center justify-center`}
       >
-        Try Nexus Designer
-      </Button>
+        <h1 className={`text-6xl text-center`}>The AI design assistant</h1>
+        <p className={`text-xl text-foreground-300`}>
+          Get answers to all your design needs
+        </p>
+        <Button
+          radius='full'
+          variant='ghost'
+          color='primary'
+          size='lg'
+        >
+          Try Nexus Designer
+        </Button>
+      </div>
     </div>
   )
 }
